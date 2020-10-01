6ef7231678197c11f7d139b0e72bde8555f489b5
Rene Rösch <r.roesch90@googlemail.com>
2020-10-01 22:59:34 +0200

    fix travis build

--- app/src/AppBundle/EventSubscriber/TwigCacheEventSubscriber.php
+++ app/src/AppBundle/EventSubscriber/TwigCacheEventSubscriber.php
@@ -6,6 +6,8 @@ use Asm89\Twig\CacheExtension\CacheProvider\PsrCacheAdapter;
 use Asm89\Twig\CacheExtension\CacheStrategy\LifetimeCacheStrategy;
 use Asm89\Twig\CacheExtension\Extension as CacheExtension;
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -28,16 +30,27 @@ class TwigCacheEventSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::REQUEST => 'registerTwigCacheExtension',
+            KernelEvents::REQUEST => 'registerTwigCacheExtensionOnRequest',
+            ConsoleEvents::COMMAND => 'registerTwigCacheExtensionOnCli',
         ];
     }
 
-    public function registerTwigCacheExtension(GetResponseEvent $event)
+    public function registerTwigCacheExtensionOnRequest(GetResponseEvent $event)
     {
         if (!$event->isMasterRequest()) {
             return;
         }
 
+        $this->addExtension();
+    }
+
+    public function registerTwigCacheExtensionOnCli(ConsoleCommandEvent $event)
+    {
+        $this->addExtension();
+    }
+
+    protected function addExtension(): void
+    {
         $cacheProvider  = new PsrCacheAdapter($this->cachePool);
         $cacheStrategy  = new LifetimeCacheStrategy($cacheProvider);
         $cacheExtension = new CacheExtension($cacheStrategy);
