013c2023636f01ad6c09f0cd2330c67e0a9756ea
Rene Rösch <r.roesch90@gmail.com>
2025-10-28 23:22:39 +0100

    more runtime fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Making sure cache, assets etc. are initialized at runtime..."
 composer auto-scripts
+chmod -R 777 var/log/*
+chmod -R 777 var/cache/*
 
 echo "Starting php-fpm..."
 exec "$@"
